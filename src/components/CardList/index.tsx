6947c2b4165a7ff91136b11a0a9ee9163633be14
viteail <vitea.sirbu1@gmail.com>
2025-04-23 12:34:31 +0300

    feat: 1 form at a time

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,61 +1,44 @@
-import { useState } from "react";
-
 import classes from "./cardList.module.css";
 
-import { Form } from "../Form";
 import { Card } from "../Card";
 
 import { TData } from "../App";
-import { IInputProps } from "../Form";
+
+export type TOnEdit = (key: string) => void;
 
 interface ICardListProps {
   type: string;
-  dataState: {
-    datas: TData[];
-    setData: React.Dispatch<React.SetStateAction<TData[]>>;
-  };
-  inputs: IInputProps[];
+  datas: TData[];
+  onEdit: TOnEdit;
 }
 
 export const CardList: React.FC<ICardListProps> = (props) => {
-  const [cardFormActive, setCardFormActive] = useState<string | null>(null);
-
-  const { dataState, type, inputs } = props;
-  const { datas } = dataState;
+  const { datas, type, onEdit } = props;
 
   let title = "";
 
   return (
     <div className={classes.cardList}>
-      {cardFormActive === null ? (
-        datas.map((data) => {
-          switch (type) {
-            case "personal":
-              title = data.fname;
-              break;
-            case "educational":
-              title = data.school;
-              break;
-            case "practical":
-              title = data.company;
-          }
+      {datas.map((data) => {
+        switch (type) {
+          case "personal":
+            title = data.fname;
+            break;
+          case "educational":
+            title = data.school;
+            break;
+          case "practical":
+            title = data.company;
+        }
 
-          return (
-            <Card
-              key={data.id}
-              title={title}
-              onEdit={() => setCardFormActive(data.id)}
-            ></Card>
-          );
-        })
-      ) : (
-        <Form
-          onClickDeactivate={() => setCardFormActive(null)}
-          dataState={dataState}
-          inputs={inputs}
-          dataInput={datas.find(data => data.id === cardFormActive)}
-        ></Form>
-      )}
+        return (
+          <Card
+            key={data.id}
+            title={title}
+            onEdit={() => onEdit(data.id)}
+          ></Card>
+        );
+      })}
     </div>
   );
 };
